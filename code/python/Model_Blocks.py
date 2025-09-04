31b873e2826b700bd998b4bc42b7f077a5bf0bcc
Guillaume Schwegler <guillaume.schwegler@unil.ch>
2025-09-04 16:48:16 +0100

    Unstable version for the NK part.

--- code/python/Model_Blocks.py
+++ code/python/Model_Blocks.py
@@ -24,12 +24,12 @@ def mkt_clearing(A, B, N, N_core, N_d):
     return asset_mkt, labor_mkt
 
 @sj.simple
-def prod(Z_core, N_core, alpha):
+def prod(Z_core, N_core, alpha, pi, mu, kappa):
     Y_core = Z_core * N_core**alpha
     w = N_core**(alpha-1)
     p_core = w / (Z_core * N_core**(alpha-1))
-    # Div = Y_core - w * N_core - mu/(mu-1)/(2*kappa) * (1+pi).apply(np.log)**2*Y_core
-    return Y_core, p_core, w
+    Div = Y_core - w * N_core - mu/(mu-1)/(2*kappa) * (1+pi).apply(np.log)**2*Y_core
+    return Y_core, p_core, Div
 
 
 @sj.simple
@@ -49,12 +49,12 @@ def prod_durables(Z_d1, Z_d2, Z_d3, Z_d4, N_d, alpha, w):
     return Y_d0, Y_d1, Y_d2, Y_d3, Y_d4, p_d0, p_d1, p_d2, p_d3, p_d4
 
 
-# @sj.simple
-# def monetary(pi, rstar, phi):
-#     r = (1 + rstar(-1) + phi * pi(-1))/(1+pi) - 1
-#     return r 
+@sj.simple
+def monetary(pi, rstar, phi):
+    r = (1 + rstar(-1) + phi * pi(-1))/(1+pi) - 1
+    return r 
 
-# @sj.simple
-# def nkpc_ss(Z_core, mu):
-#     w = Z_core / mu 
-#     return w 
+@sj.simple
+def nkpc_ss(Z_core, mu):
+    w = Z_core / mu 
+    return w 
